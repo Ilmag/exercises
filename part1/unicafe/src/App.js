1e848f5849e84e24ede6f8477142ac4ed7f0f2e7
Ilmag <50011058+Ilmag@users.noreply.github.com>
2022-08-17 11:53:59 +0400

    1.9: unicafe step4
    1.9: unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,9 +7,17 @@ const Button = (props) => {
 const Statistics = (props) => {
   const total = props.good + props.neutral + props.bad;
   const positive = props.good - props.bad;
+  if (total === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Statistics</h1>
+      <h1>statistics</h1>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
