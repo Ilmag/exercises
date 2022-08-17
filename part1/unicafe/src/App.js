c3bf30d61933396bfa4839e4f9a1dc8146921434
Ilmag <50011058+Ilmag@users.noreply.github.com>
2022-08-17 11:25:22 +0400

    1.7: unicafe step2
    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,10 +1,38 @@
 import { useState } from "react";
 
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad;
+  const positive = props.good - props.bad;
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {total}</p>
+      <p>average {positive / total}</p>
+      <p>positive {props.good / (total / 100)} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  );
 };
 
 export default App;
