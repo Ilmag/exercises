df454222c276ac94fefdfab6c383eb3daf20a059
Ilmag <50011058+Ilmag@users.noreply.github.com>
2022-08-21 18:56:34 +0400

    2.6: The Phonebook Step1
    2.6: The Phonebook Step1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNewName = (e) => {
+    console.log(newName);
+    setNewName(e.target.value);
+  };
+
+  const addNewPerson = (e) => {
+    e.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  console.log(persons);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
