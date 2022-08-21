eb8bcb8612edbc7a4ab670f9c366654581ae8259
Ilmag <50011058+Ilmag@users.noreply.github.com>
2022-08-21 11:20:56 +0400

    2.5: separate module
    2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,41 +1,5 @@
-const Header = ({ name }) => {
-  return <h2>{name}</h2>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ exercises }) => {
-  return (
-    <h3>total of {exercises.reduce((acc, curr) => acc + curr)} exersises</h3>
-  );
-};
-
-const Course = ({ course }) => {
-  const exercises = course.parts.map((part) => part.exercises);
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={exercises} />
-    </div>
-  );
-};
+// import React from "react";
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
