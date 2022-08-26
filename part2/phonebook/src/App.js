602edf1d6ff0d74b5e86acecab82fd679ad8030e
Ilmag <50011058+Ilmag@users.noreply.github.com>
2022-08-26 08:02:54 +0400

    2.11: The Phonebook Step6
    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,81 +1,86 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [filterNumber, setFilterNumber] = useState("");
-  const [filteredPersons, setFilteredPersons] = useState([]);
+    const [persons, setPersons] = useState([]);
+    const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
+    const [filterNumber, setFilterNumber] = useState("");
+    const [filteredPersons, setFilteredPersons] = useState([]);
 
-  const handleNewName = (e) => {
-    setNewName(e.target.value);
-  };
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/persons")
+            .then((response) => setPersons(response.data));
+    }, []);
 
-  const handleNewNumber = (e) => {
-    setNewNumber(e.target.value);
-  };
-
-  const handleFilterNumber = (e) => {
-    setFilterNumber(e.target.value);
-    if (e.target.value.length > 0) {
-      setFilteredPersons(
-        persons.filter((person) =>
-          person.name.toLowerCase().includes(e.target.value.toLowerCase())
-        )
-      );
-    } else {
-      setFilteredPersons(persons);
-    }
-  };
-
-  const addNewPerson = (e) => {
-    e.preventDefault();
-    const newPerson = {
-      id: persons.length + 1,
-      name: newName,
-      number: newNumber,
+    const handleNewName = (e) => {
+        setNewName(e.target.value);
     };
-    if (persons.map((person) => person.name).includes(newPerson.name)) {
-      alert(`${newPerson.name} is already added to phonebook`);
-    } else {
-      setPersons(persons.concat(newPerson));
-      setNewName("");
-      setNewNumber("");
-      setFilteredPersons(persons.concat(newPerson));
-    }
-  };
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <div>
-        <Filter
-          filterNumber={filterNumber}
-          handleFilterNumber={handleFilterNumber}
-        />
-      </div>
-      <div>
-        <h3>add a new</h3>
-        <PersonForm
-          addNewPerson={addNewPerson}
-          newName={newName}
-          handleNewName={handleNewName}
-          newNumber={newNumber}
-          handleNewNumber={handleNewNumber}
-        />
-      </div>
-      <h3>Numbers</h3>
-      <Persons filteredPersons={filteredPersons} persons={persons} />
-    </div>
-  );
+    const handleNewNumber = (e) => {
+        setNewNumber(e.target.value);
+    };
+
+    const handleFilterNumber = (e) => {
+        setFilterNumber(e.target.value);
+        if (e.target.value.length > 0) {
+            setFilteredPersons(
+                persons.filter((person) =>
+                    person.name.toLowerCase().includes(e.target.value.toLowerCase())
+                )
+            );
+        } else {
+            setFilteredPersons(persons);
+        }
+    };
+
+    const addNewPerson = (e) => {
+        e.preventDefault();
+        const newPerson = {
+            id: persons.length + 1,
+            name: newName,
+            number: newNumber,
+        };
+        if (persons.map((person) => person.name).includes(newPerson.name)) {
+            alert(`${newPerson.name} is already added to phonebook`);
+        } else {
+            setPersons(persons.concat(newPerson));
+            setNewName("");
+            setNewNumber("");
+            setFilteredPersons(persons.concat(newPerson));
+        }
+    };
+
+    return ( <
+        div >
+        <
+        h2 > Phonebook < /h2>{" "} <
+        div >
+        <
+        Filter filterNumber = { filterNumber }
+        handleFilterNumber = { handleFilterNumber }
+        />{" "} <
+        /div>{" "} <
+        div >
+        <
+        h3 > add a new < /h3>{" "} <
+        PersonForm addNewPerson = { addNewPerson }
+        newName = { newName }
+        handleNewName = { handleNewName }
+        newNumber = { newNumber }
+        handleNewNumber = { handleNewNumber }
+        />{" "} <
+        /div>{" "} <
+        h3 > Numbers < /h3>{" "} <
+        Persons filteredPersons = { filteredPersons }
+        persons = { persons }
+        />{" "} <
+        /div>
+    );
 };
 
 export default App;
